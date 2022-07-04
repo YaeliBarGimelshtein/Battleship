70029b23259ecdc6262b0e6f31946b4a606b1339
YaeliBarGimelshtein <yaeli47@gmail.com>
2022-07-04 19:29:53 +0200

    startted server

--- Server.py
+++ Server.py
@@ -1,2 +1,10 @@
-print("hello world")
+import socket
+import threading
+
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+BIND = (SERVER, PORT)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(BIND)
 
